d6d5d13328f1f34829970d9cc0b471600e31741c
minki-jeon <minki.jeon.study@gmail.com>
2025-07-01 14:58:10 +0900

    React Bootstrap

---
+++ src/App16.jsx
@@ -0,0 +1,16 @@
+import { Button } from "react-bootstrap";
+
+function App16() {
+  return (
+    <>
+      <button className={"btn btn-primary"}>저장</button>
+      <Button>등록</Button>
+      <Button variant="warning">경고</Button>
+      <Button variant="danger">위험</Button>
+      <Button>버튼 </Button>
+      <hr />
+    </>
+  );
+}
+
+export default App16;
