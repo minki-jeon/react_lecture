2e81a3ece6e7eb49d106035ecd1c154a6913a30f
minki-jeon <minki.jeon.study@gmail.com>
2025-07-01 15:48:35 +0900

    React icons / 동일한 prop은 별칭 사용

--- src/App17.jsx
+++ src/App17.jsx
@@ -1,9 +1,16 @@
 import { FaMugHot } from "react-icons/fa6";
+import { BiLogoFacebookCircle } from "react-icons/bi";
+import { HiBookmark as Bookmark1 } from "react-icons/hi";
+import { HiBookmark as Bookmark2 } from "react-icons/hi2";
 
 function App17() {
   return (
     <div>
       <FaMugHot />
+
+      <BiLogoFacebookCircle />
+      <Bookmark1 />
+      <Bookmark2 />
     </div>
   );
 }
