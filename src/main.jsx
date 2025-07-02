4f824c57ecfc11055f01ce8946973fd5eeb77e1c
minki-jeon <minki.jeon.study@gmail.com>
2025-07-02 18:12:39 +0900

    React useState

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import App18 from "./App18.jsx";
+import App31 from "./App31.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App18 />
+    <App31 />
   </StrictMode>,
 );
