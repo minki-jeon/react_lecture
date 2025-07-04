2f3412e63d7e7fabcd1c86cf5c1af60ddb5520ca
minki-jeon <minki.jeon.study@gmail.com>
2025-07-04 11:19:23 +0900

    React nested routes, route index

--- src/App44.jsx
+++ src/App44.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
+// /learn : 웹앱 개발 배우기
 // /learn/react : 리액트 배우기
 // /learn/spring : 스프링 배우기
 // /learn/java/collection : 자바 콜렉션 배우기
@@ -11,12 +12,16 @@ function App44(props) {
       <BrowserRouter>
         <Routes>
           <Route path="learn">
+            <Route index element={<h3>웹앱 배우기</h3>} />
             <Route path="react" element={<h3>리액트 배우기</h3>}></Route>
             <Route path="spring" element={<h3>스프링 배우기</h3>}></Route>
-            <Route
-              path="java/collection"
-              element={<h3>자바 콜렉션 배우기</h3>}
-            ></Route>
+            <Route path="java">
+              <Route index element={<h3>자바 배우기</h3>} />
+              <Route
+                path="collection"
+                element={<h3>java collection 배우기</h3>}
+              />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
