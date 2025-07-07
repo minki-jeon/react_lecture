47e60563ff62473b9806dc3765d6e8da5043ccb2
minki-jeon <minki.jeon.study@gmail.com>
2025-07-07 11:39:47 +0900

    axios-queryString/useState+get request 4가지방법

--- src/App53.jsx
+++ src/App53.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App53(props) {
+  // use...(hook)들은 컴포넌트 최상단에 작성( 조건문, 반복문 사용금지 )
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [city, setCity] = useState("");
+
+  const [fruit, setFruit] = useState("");
+  const [price, setPrice] = useState(0);
+  const [location, setLocation] = useState("");
+
   function handleButton1Click() {
     axios.request({ method: "get", url: "http://localhost:8080" });
   }
@@ -97,8 +106,133 @@ function App53(props) {
     axios.get(`/api/main28/sub16?${p}`);
   }
 
+  function handleButton17Click() {
+    // query string 직접 완성
+    axios.get(`/api/main28/sub17?name=${name}&age=${age}&city=${city}`);
+  }
+  function handleButton18Click() {
+    // URLSearchParams를 연결연산자로 붙이기
+    const p = new URLSearchParams();
+    p.set("name", name);
+    p.set("age", age);
+    p.set("city", city);
+    // axios.get(`/api/main28/sub18?` + p);
+    axios.get(`/api/main28/sub18?${p}`);
+  }
+  function handleButton19Click() {
+    // URLSearchParams를 config에 넣기
+    const p = new URLSearchParams();
+    p.set("name", name);
+    p.set("age", age);
+    p.set("city", city);
+    // axios.get(`/api/main28/sub18?` + p);
+    axios.get("/api/main28/sub19?", { params: p });
+  }
+  function handleButton20Click() {
+    // 객체를 config.params에 넣기
+    axios.get("/api/main28/sub20", {
+      params: {
+        name: name,
+        age: age,
+        city: city,
+      },
+    });
+  }
+
+  function handleButton21Click() {
+    axios.get(
+      `/api/main28/sub21?fruit=${fruit}&price=${price}&location=${location}`,
+    );
+  }
+
+  function handleButton22Click() {
+    const p = new URLSearchParams();
+    p.set("fruit", fruit);
+    p.set("price", price);
+    p.set("location", location);
+    axios.get(`/api/main28/sub22?${p}`);
+  }
+
+  function handleButton23Click() {
+    const p = new URLSearchParams();
+    p.set("fruit", fruit);
+    p.set("price", price);
+    p.set("location", location);
+    axios.get(`/api/main28/sub23`, { params: p });
+  }
+
+  function handleButton24Click() {
+    axios.get("/api/main28/sub24", {
+      params: {
+        fruit: fruit,
+        price: price,
+        location: location,
+      },
+    });
+  }
+
   return (
     <div>
+      <hr />
+      <div>
+        <div>
+          <input
+            type="text"
+            value={fruit}
+            onChange={(e) => setFruit(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <button onClick={handleButton24Click}>요청 24</button>
+      <button onClick={handleButton23Click}>요청 23</button>
+      <button onClick={handleButton22Click}>요청 22</button>
+      <button onClick={handleButton21Click}>요청 21</button>
+      <hr />
+
+      <div>
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+      </div>
+      <button onClick={handleButton20Click}>요청 20</button>
+      <button onClick={handleButton19Click}>요청 19</button>
+      <button onClick={handleButton18Click}>요청 18</button>
+      <button onClick={handleButton17Click}>요청 17</button>
+
+      <hr />
       <button onClick={handleButton16Click}>
         요청 16 URLSearchParams 객체 활용{" toString "}
       </button>
