71e0a8969b1c2d663f10095bca366c087304c439
minki-jeon <minki.jeon.study@gmail.com>
2025-07-07 10:31:30 +0900

    axios - queryString / config.params:{}

--- src/App53.jsx
+++ src/App53.jsx
@@ -31,8 +31,44 @@ function App53(props) {
     axios.get("/api/main28/sub7");
   }
 
+  function handleButton8Click() {
+    axios.get("/api/main28/sub8?name=son&age=33");
+  }
+
+  function handleButton9Click() {
+    axios.get("/api/main28/sub9?address=seoul&score=88.15");
+  }
+
+  function handleButton10Click() {
+    // query string 직접 입력
+    axios.get("/api/main28/sub10?address=서울 강남&score=88.15");
+  }
+
+  function handleButton11Click() {
+    // query string 입력 대신 config.params에 객체 대입 (자동 encoding)
+    axios.get("/api/main28/sub11", {
+      params: { address: "서울 신촌", score: "77.77" },
+    });
+  }
+
+  function handleButton12Click() {
+    axios.get("/api/main28/sub12", {
+      params: { fruit: "apple", price: "6" },
+    });
+  }
+
   return (
     <div>
+      <button onClick={handleButton12Click}>요청 12 + query string</button>
+      <hr />
+      <button onClick={handleButton11Click}>요청 11 + query string</button>
+      <hr />
+      <button onClick={handleButton10Click}>요청 10 + query string</button>
+      <hr />
+      <button onClick={handleButton9Click}>요청 9 + query string</button>
+      <hr />
+      <button onClick={handleButton8Click}>요청 8 w/ query string</button>
+      <hr />
       <button onClick={handleButton7Click}>요청 보내기 7</button>
       <hr />
       <button onClick={handleButton6Click}>요청 보내기 6</button>
