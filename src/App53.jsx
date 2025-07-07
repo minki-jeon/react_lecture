84491c3866e5cc47ac784075fb6bf8f103b959e4
minki-jeon <minki.jeon.study@gmail.com>
2025-07-07 09:54:59 +0900

    axios - java spring request (get,post) .request() .get()

--- src/App53.jsx
+++ src/App53.jsx
@@ -22,8 +22,21 @@ function App53(props) {
     axios.request({ method: "post", url: "/api/main28/sub4" });
   }
 
+  function handleButton6Click() {
+    // axios.request({ method: "get", url: "/api/main28/sub6" });
+    axios.get("/api/main28/sub6");
+  }
+
+  function handleButton7Click() {
+    axios.get("/api/main28/sub7");
+  }
+
   return (
     <div>
+      <button onClick={handleButton7Click}>요청 보내기 7</button>
+      <hr />
+      <button onClick={handleButton6Click}>요청 보내기 6</button>
+      <hr />
       <button onClick={handleButton5Click}>요청 보내기 5</button>
       <hr />
       <button onClick={handleButton4Click}>요청 보내기 4</button>
