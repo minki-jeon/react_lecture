b0223138f85606d74599859a34e68c4f781ce936
minki-jeon <minki.jeon.study@gmail.com>
2025-07-08 17:26:34 +0900

    axios Response

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import App55 from "./App55.jsx";
+import App57 from "./App57.jsx";
 
 createRoot(document.getElementById("root")).render(
   // strict mode : 렌더링할 때, 모든 컴포넌트를 [마운트->언마운트->마운트] 과정을 수행한다.
   // <StrictMode>
-  <App55 />,
+  <App57 />,
   // </StrictMode>,
 );
