e9976da23fb68f09b59bdee375c61a81cc4fb07a
minki-jeon <minki.jeon.study@gmail.com>
2025-07-08 09:40:15 +0900

    axios postForm request File(Multiple)

---
+++ src/App56.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App56(props) {
+  const [myFiles, setMyFiles] = useState(null);
+
+  function handleButton1Click() {
+    // file 전송 시 postForm/putForm
+    axios.postForm("/api/main31/sub1", {
+      name: "흥민",
+      age: 77,
+      myFiles: myFiles,
+    });
+  }
+
+  function hanldeButton2Click() {
+    axios.postForm("/api/main31/sub2", {
+      address: "신촌",
+      score: 55.66,
+      yourFiles: myFiles,
+    });
+  }
+
+  function handleButton3Click() {
+    axios.postForm("/api/main31/sub3", {
+      city: "강남",
+      uploadFiles: myFiles,
+    });
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="file"
+          multiple={true}
+          onChange={(e) => setMyFiles(e.target.files)}
+        />
+        <button onClick={handleButton3Click}>파일 전송 3 여러개</button>
+      </div>
+      <div>
+        <input
+          type="file"
+          multiple={true}
+          onChange={(e) => setMyFiles(e.target.files)}
+        />
+        <button onClick={hanldeButton2Click}>파일 전송 2 여러개</button>
+      </div>
+      <div>
+        {/* e.target.files */}
+        <input type="file" onChange={(e) => setMyFiles(e.target.files)} />
+        <button onClick={handleButton1Click}>파일 전송 1</button>
+      </div>
+    </div>
+  );
+}
+
+export default App56;
